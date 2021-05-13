762260c71c2b489450d88e1d67677335257c5568
sairamaj <sairamaj@gmail.com>
2021-05-12 20:57:32 -0700

    initial checkin

---
+++ ClientCredential/src/Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task CallApi(string accessToken)
+        {
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(accessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:7001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+
+        static async Task<string> Test()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return null;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return null;
+            }
+
+            Console.WriteLine(tokenResponse.AccessToken);
+            return tokenResponse.AccessToken;
+        }
+        static void Main(string[] args)
+        {
+            var token = Test().Result;
+            CallApi(token).Wait();
+        }
+    }
+}
